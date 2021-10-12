70dc6e9d30d9622e9c852081aa4bcdf2da3cbfd8
jhill1971 <1971jamesh@gmail.com>
2021-10-11 20:33:50 -0400

    Basic UI and functionality implemented. 10-11-2021

---
+++ app/src/main/java/com/jameshill/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,170 @@
+package com.jameshill.happyplaces
+
+import android.Manifest
+import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.jameshill.happyplaces.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    /**
+     * An variable to get an instance calendar using the default time zone and locale.
+     */
+    private var cal = Calendar.getInstance()
+
+    /**
+     * A variable for DatePickerDialog OnDateSetListener.
+     * The listener used to indicate the user has finished selecting a date. Which we will be initialize later on.
+     */
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_add_happy_place)
+
+        setSupportActionBar(toolbar_addPlace) // Use the toolbar to set the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // This is to use the home back button.
+        // Setting the click event to the back button
+        toolbar_addPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        // https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/
+        // create an OnDateSetListener
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                updateDateInView()
+            }
+
+        et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener, // This is the variable which have created globally and initialized in setupUI method.
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR), // Here the cal instance is created globally and used everywhere in the class where it is required.
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+
+
+            R.id.tv_add_image -> {
+                val pictureDialog = AlertDialog.Builder(this)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems =
+                    arrayOf("Select photo from gallery", "Capture photo from camera")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { dialog, which ->
+                    when (which) {
+                        // Here we have create the methods for image selection from GALLERY
+                        0 -> choosePhotoFromGallery()
+                        1 -> Toast.makeText(this@AddHappyPlaceActivity,"Camera selection coming soon...", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                pictureDialog.show()
+            }
+
+        }
+    }
+
+    /**
+     * A function to update the selected date in the UI with selected format.
+     * This function is created because every time we don't need to add format which we have added here to show it in the UI.
+     */
+    private fun updateDateInView() {
+        val myFormat = "MM.dd.yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // A date format
+        et_date.setText(sdf.format(cal.time).toString()) // A selected date using format which we have used is set to the UI.
+    }
+
+
+    /**
+     * A method is used for image selection from GALLERY / PHOTOS of phone storage.
+     */
+    private fun choosePhotoFromGallery() {
+
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                    // Here after all the permission are granted launch the gallery to select and image.
+                    if (report!!.areAllPermissionsGranted()) {
+
+                        Toast.makeText(this@AddHappyPlaceActivity,"Storage READ/WRITE permission are granted. Now you can select an image from GALLERY or lets says phone storage.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+        // END
+    }
+    // END
+
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog,
+                                           _ ->
+                dialog.dismiss()
+            }.show()
+    }
+    // END
+}
